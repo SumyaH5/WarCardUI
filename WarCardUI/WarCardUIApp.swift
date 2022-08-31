cfbe489f1f41e9d88187034f811cf7b4839dbbe7
Sumya Hoque <mhoque442@gmail.com>
2022-08-31 18:51:40 -0400

    Initial Commit

---
+++ WarCardUI/WarCardUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardUIApp.swift
+//  WarCardUI
+//
+//  Created by Sumo Hoque on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
